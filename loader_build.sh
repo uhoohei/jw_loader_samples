f9c6c69d2ed9eb1d6a8cfba0db32fbd61624aab6
Justin Wong <justinlikefish@qq.com>
2017-06-06 20:39:01 +0800

    修改项目编译脚本

--- loader_build.sh
+++ loader_build.sh
@@ -5,7 +5,8 @@
 MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MY_DEST_DIR=$MY_DIR/res
 
-source $MY_DIR/../jw_loader/build.sh
+# TODO: 修改下面的目录为你的jw_loader项目的实际目录
+source $QUICK_V3_ROOT/../jw_loader/build_luajit.sh $1
 
-echo "mv $MY_DIR/../jw_loader/loader.zip $MY_DEST_DIR/"
-mv $MY_DIR/../jw_loader/loader.zip $MY_DEST_DIR/
+echo "mv $QUICK_V3_ROOT/../jw_loader/*.zip $MY_DEST_DIR/"
+mv $QUICK_V3_ROOT/../jw_loader/*.zip $MY_DEST_DIR/
